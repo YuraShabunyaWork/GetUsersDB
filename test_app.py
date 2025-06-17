7ece03df3c55ee3a5ae8b5dd15b3541b23089965
YuraShabunyaWork <yura.shabunya.work@gmail.com>
2025-06-17 14:57:28 +0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,5 +2,5 @@ def test_home():
     from app import app
     client = app.test_client()
     response = client.get('/')
-    assert response.status_code == 200
+    assert response.status_code == 400
 
