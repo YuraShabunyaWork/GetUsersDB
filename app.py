08718cecd606fe28c3216b163f471ca3e9395bba
YuraShabunyaWork <yura.shabunya.work@gmail.com>
2025-06-17 14:46:37 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from flask import Flask, jsonify
 import psycopg2
 
 app = Flask(__name__)
-conn = psycopg2.connect("dbname=demo user=user password=pass host=db")
+
+def get_connection():
+    return psycopg2.connect("dbname=demo user=user password=pass host=db")
 
 @app.route("/")
 def hello():
@@ -10,12 +12,13 @@ def hello():
 
 @app.route('/users')
 def get_users():
+    conn = get_connection()
     cur = conn.cursor()
     cur.execute("SELECT * FROM users")
     rows = cur.fetchall()
     cur.close()
-    return jsonify(rows)  # Убрали time.sleep()
+    conn.close()
+    return jsonify(rows)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
